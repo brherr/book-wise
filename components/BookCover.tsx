863b9bfa31ac84632705397023d23c1be765dbdb
Brad Herr <herrbrad@yahoo.com>
2025-04-29 12:46:41 -0400

    home data start

---
+++ components/BookCover.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+type BookCoverVariant = "extraSmall" | "small" | "medium" | "regular" | "wide";
+
+const variantStyles: Record<BookCoverVariant, string> = {
+  extraSmall: "book-cover_extra_small",
+  small: "book-cover_small",
+  medium: "book-cover_medium",
+  regular: "book-cover_regular",
+  wide: "book-cover_wide",
+};
+
+interface Props {
+  className?: string;
+  variant: BookCoverVariant;
+  coverColor: string;
+  coverUrl: string;
+}
+
+const BookCover = ({ className, variant, coverColor, coverUrl }: Props) => {
+  return <div>BookCover</div>;
+};
+
+export default BookCover;
